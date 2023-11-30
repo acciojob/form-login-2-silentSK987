dad7ec43e36b314ff0bc2fc412bd5a96201f0328
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-11-30 12:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const form =document.getElementById("form");
+const submit =document.getElementById("submit");
+
+let data=[];
+
+const onSubmitForm = (event)=>{
+    event.preventDefault(); 
+    const empdata={
+        firstName: event.target.firstname.value,
+        lastName: event.target.lastname.value,
+        phoneNumber: event.target.phone.value,
+        email: event.target.email.value,
+        
+    }
+    let fname=`First Name: ${empdata.firstName}
+Last Name: ${empdata.lastName}
+Phone Number: ${empdata.phoneNumber} 
+Email ID: ${empdata.email}`
+   alert(fname);
+}
+	
+form.addEventListener("submit", onSubmitForm);
